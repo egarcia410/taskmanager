8de66b506e89cbe06bf213a481d2566cc71331f8
egarcia410 <egarcia410@gmail.com>
2016-11-06 12:59:31 -0600

    Add: Templates and CRUD functionality

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
-## Task Manager
+# Task Manager
 
+### Description
+Web application that uses JQuery along with mLab data API. In this application, you will be able to create tasks and categories as well as update and delete them.
 
-## Setting up mLab
-
-1. Signup for (mLab)[https://mlab.com/]
-2. Once logged in, you will be redirected to the (home)[https://mlab.com/home] page.
+### Setting up mLab
+1. Signup for [mLab](https://mlab.com/)
+2. Once logged in, you will be redirected to the [home](https://mlab.com/home) page.
 3. At the home page, click 'Create new'. This will create a new deployment from scratch.
 4. Choose Cloud Provider. Default is AWS.
 5. Choose Plan. The free plan is under Single-node tab, then check Sandbox.
 6. Give a Database name, `taskmanager`.
-7. Click 'Create new MongoDB deployment'
-8. Click on your newly created project
-9. Click on the Users tab and 'Add database user'
-10. Under Collections tab, click 'Add collection'
-11. Create two collections called 'categories' and 'task'.
-    >Note: (Optional) Add documents to the collections
+7. Click 'Create new MongoDB deployment'.
+8. Click on your newly created project.
+9. Click on the Users tab and 'Add database user'.
+10. Under Collections tab, click 'Add collection'.
+11. Create two collections called `categories` and `task`.
 12. Retreive API key from mLab and Enable data API access.
-13. Insert API key on line #1 in the main.js file.
+13. Insert API key on line #16 in the main.js file.
+
+### Setting up project
+1. Clone project repository: `$ https://github.com/egarcia410/taskmanager.git`
+2. Open project folder with text editor
+3. Edit `main.js`, Insert API key on line #16
+4. Open `index.html` inside browser
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,30 @@
+$(document).ready(function(){
+    $('#add_category').submit(addCategory);
+    $('#edit_category').submit(editCategory);
+
+    $('#add_task').submit(addTask);
+    $('#edit_task').submit(editTask);
+
+    $('body').on('click', '.btn-edit-category', setCategory);
+    $('body').on('click', '.btn-delete-category', deleteCategory);
+
+    $('body').on('click', '.btn-edit-task', setTask);
+    $('body').on('click', '.btn-delete-task', deleteTask);
+});
+
+// Edit line below with your personal api key generated from mLab
 var api_key = 'u1eaJG2HTmfGAQ26AfcAShz7ZV-Lj5ml';
+
+function getCategoryOptions(){
+    $.get( "https://api.mongolab.com/api/1/databases/taskmanager/collections/categories?apiKey="+api_key, function(data){
+        var output;
+        $.each(data, function(key, data){
+            output += '<option value="'+data.category_name+'">'+data.category_name+'</option>';
+        })
+        $("#category").append(output);
+    });
+}
+
 
 function getCategories(){
     $.get('https://api.mlab.com/api/1/databases/taskmanager/collections/categories?apiKey='+api_key, function(data){
@@ -12,3 +38,185 @@ function getCategories(){
         $('#categories').html(output)
     });
 }
+
+
+function addCategory(){
+    var category_name = $('#category_name').val();
+    $.ajax({
+        url: "https://api.mlab.com/api/1/databases/taskmanager/collections/categories?apiKey="+api_key,
+        data: JSON.stringify({
+            'category_name': category_name
+        }),
+        type: 'POST',
+        contentType: 'application/json',
+        success: function(data){
+            window.location.href = 'categories.html';
+        },
+        error: function(xhr, status, err){
+            console.log(err);
+        }
+    });
+    return false
+};
+
+function editCategory(){
+    var category_id = sessionStorage.getItem('currentCategoryId');
+    var category_name = $('#category_name').val();
+    $.ajax({
+        url: "https://api.mlab.com/api/1/databases/taskmanager/collections/categories/"+category_id+"?apiKey="+api_key,
+        data: JSON.stringify({
+            'category_name': category_name
+        }),
+        type: 'PUT',
+        contentType: 'application/json',
+        success: function(data){
+            window.location.href = 'categories.html';
+        },
+        error: function(xhr, status, err){
+            console.log(err);
+        }
+    });
+    return false
+};
+
+function deleteTask(){
+    var task_id = $(this).data('task-id');
+    $.ajax({
+        url: 'https://api.mongolab.com/api/1/databases/taskmanager/collections/tasks/'+task_id+'?apiKey='+api_key,
+        type: "DELETE",
+        async: true,
+        timeout: 300000,
+        success: function (data) {
+            window.location.href = "index.html";
+        },
+        error: function (xhr, status, err) {
+            console.log(err);
+        }
+    });
+
+    return false;
+}
+
+function getTasks(){
+    $.get( "https://api.mongolab.com/api/1/databases/taskmanager/collections/tasks?apiKey="+api_key, function(data){
+        var output = '<ul class="list-group">';
+        $.each(data, function(key, data){
+            output += '<li class="task list-group-item">';
+            output += data.task_name+' <p><span class="due_on">[Due on '+data.due_date+']</span></p>';
+                if(data.is_urgent == "yes"){
+                    output += '<p><span class="label label-danger">Urgent</span></p>';
+                }
+            output += '<div><a class="btn btn-primary btn-edit-task" data-task-name="'+data.task_name+'" data-task-id="'+data._id.$oid+'">Edit</a> <a class="btn btn-danger btn-delete-task" data-task-id="'+data._id.$oid+'">Delete</a></div>';
+            output += '</li>';
+        })
+        output += '</ul>';
+        $("#tasks").html(output);
+    });
+}
+
+function addTask(){
+    var task_name = $('#task_name').val();
+    var category = $('#category').val();
+    var due_date = $('#due_date').val();
+    var is_urgent = $('#is_urgent').val();
+    $.ajax({ url: "https://api.mongolab.com/api/1/databases/taskmanager/collections/tasks?apiKey="+api_key,
+        data: JSON.stringify({
+            "task_name" : task_name,
+            "category": category,
+            "due_date": due_date,
+            "is_urgent": is_urgent
+        }),
+        type: "POST",
+        contentType: "application/json",
+        success: function(data){
+            window.location.href = "index.html";
+        },
+        error: function (xhr, status, err) {
+            console.log(err);
+        }
+    });
+    return false;
+}
+
+function editTask(){
+    var task_id = sessionStorage.getItem('currentTaskId');
+    console.log(task_id);
+    var task_name = $('#task_name').val();
+    var category = $('#category').val();
+    var due_date = $('#due_date').val();
+    var is_urgent = $('#is_urgent').val();
+    $.ajax({url: 'https://api.mongolab.com/api/1/databases/taskmanager/collections/tasks/'+task_id+'?apiKey='+api_key,
+        data: JSON.stringify({
+            "task_name" : task_name,
+            "category": category,
+            "due_date": due_date,
+            "is_urgent": is_urgent
+        }),
+        type: "PUT",
+        contentType: "application/json",
+        success: function(data){
+            window.location.href = "index.html";
+        },
+        error: function (xhr, status, err) {
+            console.log(err);
+        }
+    });
+    return false;
+}
+
+function setTask(){
+    var task_id = $(this).data('task-id');
+    console.log('Task Set: '+task_id);
+    sessionStorage.setItem('currentTaskId', task_id);
+    window.location.href = "edittask.html";
+    return false;
+}
+
+function getTask(){
+    var task_id = sessionStorage.getItem('currentTaskId');
+    $.get( 'https://api.mongolab.com/api/1/databases/taskmanager/collections/tasks/'+task_id+'?apiKey='+api_key, function(data){
+        $('#task_name').val(data.task_name);
+        $('#category').val(data.category);
+        $('#due_date').val(data.due_date);
+        if(data.is_urgent == "yes"){
+            $('#is_urgent').prop('selected', true);
+        }
+    });
+    return false;
+}
+
+function setCategory(){
+    var category_id = $(this).data('category-id');
+    sessionStorage.setItem('currentCategoryId', category_id);
+    window.location.href = 'editcategory.html';
+    return false
+};
+
+
+function getCategory(){
+    var category_id = sessionStorage.getItem('currentCategoryId');
+    $.get('https://api.mlab.com/api/1/databases/taskmanager/collections/categories/'+category_id+'?apiKey='+api_key, function(data){
+        $('#category_name').val(data.category_name);
+    });
+};
+
+function deleteCategory(){
+    var category_id = $(this).data('category-id');
+    $.ajax({
+        url: "https://api.mlab.com/api/1/databases/taskmanager/collections/categories/"+category_id+"?apiKey="+api_key,
+        type: 'DELETE',
+        async: true,
+        timeout: 300000,
+        success: function(data){
+            window.location.href = 'categories.html';
+        },
+        error: function(xhr, status, err){
+            console.log(err);
+        }
+    });
+    return false
+}
+
+
+
+
